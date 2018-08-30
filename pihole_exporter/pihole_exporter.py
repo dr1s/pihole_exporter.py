b14c73de8174de14dd9aadf0da72fe11a9d1a582
dr1s <dr1s@users.noreply.github.com>
2018-08-30 23:50:35 +0700

    Set metric to 0 if it's not exported by pihole anymore

--- pihole_exporter/pihole_exporter.py
+++ pihole_exporter/pihole_exporter.py
@@ -26,6 +26,7 @@ class pihole_exporter:
         self.api_url = 'http://%s/admin/api.php' % self.url
         self.metrics = dict()
         self.httpd = None
+        self.metrics_collected = dict()
 
         self.summary_raw_url = self.api_url + '?summaryRaw'
         self.top_item_url = self.api_url + '?topItems'
@@ -44,11 +45,15 @@ class pihole_exporter:
         return json_text
 
     def get_summary(self, url):
+
         summary_raw = self.get_json(url)
+        metrics_collected = dict()
+
         for i in summary_raw:
             if not i in self.metrics:
                 self.metrics[i] = Gauge('pihole_%s' % i.lower(),
                                         i.replace('_',' '))
+            metrics_collected[i] = True
             if i == "status":
                 if summary_raw[i] == 'enabled':
                     self.metrics[i].set(1)
@@ -59,39 +64,92 @@ class pihole_exporter:
                 self.metrics[i].set(summary_raw[i]['absolute'])
             else:
                 self.metrics[i].set(summary_raw[i])
+        return metrics_collected
 
     def convert_json(self, json_data, name, option):
+        metrics_collected = dict()
         for i in json_data:
             if name not in self.metrics:
                 self.metrics[name] = Gauge( 'pihole_%s' % name,
                                     name.replace('_', ' '),
                                     [ option ])
             self.metrics[name].labels(i).set(json_data[i])
+            metrics_collected[i] = True
+        return metrics_collected
+
+    def _merge_collected_metrics(self, metrics_collected):
+
+        if len(self.metrics_collected) == 0:
+            self.metrics_collected = metrics_collected
+        else:
+
+            for l in self.metrics_collected:
+
+                if not type(self.metrics_collected[l]) is dict:
+                    if not l in metrics_collected:
+                        self.metrics_collected[l] = False
+                        self.metrics[l].set('0')
+                    else:
+                        self.metrics_collected[l] = True
+
+                else:
+
+                    for m in self.metrics_collected[l]:
+                        if not m in metrics_collected[l]:
+                            self.metrics_collected[l][m] = False
+                            self.metrics[l].labels(m).set('0')
+                        else:
+                            self.metrics_collected[l][m] = True
+
+            for l in metrics_collected:
+
+                if not type(metrics_collected[l]) is dict:
+                    if not l in self.metrics_collected:
+                        self.metrics_collected[l] = metrics_collected[l]
+                else:
+                    for m in metrics_collected[l]:
+                        if not m in self.metrics_collected[l]:
+                            self.metrics_collected[l][m] = metrics_collected[l][m]
+
+
 
     def get_metrics(self):
-        self.get_summary(self.summary_raw_url)
+        metrics_collected = self.get_summary(self.summary_raw_url)
 
         top_items = self.get_json(self.top_item_url)
         if top_items:
             top_queries = top_items['top_queries']
-            self.convert_json(top_queries, 'top_queries', 'domain')
+            metrics_collected['top_queries'] = self.convert_json(
+                                                        top_queries,
+                                                        'top_queries',
+                                                        'domain')
             top_ads = top_items['top_ads']
-            self.convert_json(top_ads, 'top_ads', 'domain')
+            metrics_collected['top_ads'] = self.convert_json(top_ads,
+                                                            'top_ads',
+                                                            'domain')
 
         top_sources = self.get_json(self.top_sources_url)
         if top_sources:
-            self.convert_json(  top_sources['top_sources'],
-                                'top_sources',
-                                'client')
+            metrics_collected['top_sources'] = self.convert_json(
+                                                top_sources['top_sources'],
+                                                'top_sources',
+                                                'client')
 
         fw_dest = self.get_json(self.forward_destinations_url)
         if fw_dest:
-            self.convert_json(fw_dest['forward_destinations'],
-                        'forward_destinations', 'resolver')
+            metrics_collected['forward_destinations'] = self.convert_json(
+                                                fw_dest['forward_destinations'],
+                                                'forward_destinations',
+                                                'resolver')
 
         qt = self.get_json(self.query_types_url)
         if qt:
-            self.convert_json(qt['querytypes'], 'query_type', 'type')
+            metrics_collected['query_type'] = self.convert_json(
+                                                qt['querytypes'],
+                                                'query_type',
+                                                'type')
+
+        self._merge_collected_metrics(metrics_collected)
 
         return generate_latest()
 
@@ -111,7 +169,7 @@ class pihole_exporter:
         if ':' in interface:
             if getattr(server_class, 'address_family') == socket.AF_INET:
                     server_class.address_family = socket.AF_INET6
-                    
+
         print("* Listening on %s:%s" % (interface, port))
         self.httpd = make_server(   interface,
                                     port,
